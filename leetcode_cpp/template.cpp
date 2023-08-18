c21e10c73e483ca88c428f45e2fdd43ce3512ecb
Josh Kabo <joshkong1@gmail.com>
2023-08-18 13:02:06 -0700

    more mindful of leetcode usage

---
+++ leetcode_cpp/template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <regex>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <iomanip>
+#include <utility>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int problem(int input)
+    {
+        return 0;
+    }
+};
+
+/*
+
+*/
