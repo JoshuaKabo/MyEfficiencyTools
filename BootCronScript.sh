22cf063fa1cd9945cd2f6070e400b6e4256ce444
JoshuaKabo <joshkong1@gmail.com>
2025-05-28 12:30:12 -0700

    Configuration uses crontab, includes notes

---
+++ BootCronScript.sh
@@ -0,0 +1,7 @@
+xremap /home/joshk/dev/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
+# How to use:
+# Install xremap
+# Install crontab (whatever flavor, EOS seems to use cronie)
+# Activate crontab using sysctl
+    # Just because it's installed doesn't mean it's active
+# use "sudo crontab -e" to make an entry for "@reboot /home/joshk/dev/MyEfficiencyTools/BootCronScript.sh"
