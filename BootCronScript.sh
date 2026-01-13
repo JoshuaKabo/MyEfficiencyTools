bea607c9749c2d1bc1ca2aab4100441087bdedf6
Josh Kabo <joshkong1@gmail.com>
2026-01-13 11:16:09 -0800

    Fix file paths and update crontab instructions
    Updated file paths and instructions for crontab activation.

--- BootCronScript.sh
+++ BootCronScript.sh
@@ -1,7 +1,9 @@
-xremap /home/joshk/dev/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
+xremap /home/joshk/MyDevStuff/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
 # How to use:
 # Install xremap
 # Install crontab (whatever flavor, EOS seems to use cronie (yay cron, get cronie))
-# Activate crontab using sysctl
+# Activate crontab using systemctl
     # Just because it's installed doesn't mean it's active
-# use "sudo crontab -e" to make an entry for "@reboot /home/joshk/dev/MyEfficiencyTools/BootCronScript.sh"
+    # sudo systemctl enable cronie
+    # sudo systemctl start cronie
+# use "crontab -e" to make an entry for "@reboot /home/joshk/MyDevStuff/MyEfficiencyTools/BootCronScript.sh"
