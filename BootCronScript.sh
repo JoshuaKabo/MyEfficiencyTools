aa03ae63dd847cbc3befa94ad81f3e291a84f223
Josh Kabo <joshkong1@gmail.com>
2026-01-13 11:05:23 -0800

    Update BootCronScript.sh

--- BootCronScript.sh
+++ BootCronScript.sh
@@ -1,7 +1,7 @@
 xremap /home/joshk/dev/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
 # How to use:
 # Install xremap
-# Install crontab (whatever flavor, EOS seems to use cronie)
+# Install crontab (whatever flavor, EOS seems to use cronie (yay cron, get cronie))
 # Activate crontab using sysctl
     # Just because it's installed doesn't mean it's active
 # use "sudo crontab -e" to make an entry for "@reboot /home/joshk/dev/MyEfficiencyTools/BootCronScript.sh"
