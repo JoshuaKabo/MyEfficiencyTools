0923fbab4f6de461cb88a75eecc409e7354e62e0
Josh Kabo <mail@joshkabo.xyz>
2026-08-23 22:38:59 -0700

    a few minor path and semantics fixes I think I didnt check diff

--- BootCronScript.sh
+++ BootCronScript.sh
@@ -1,6 +1,8 @@
-bash xremap /home/joshk/Projects/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
+echo $PATH
+/home/joshk/.cargo/bin/xremap /home/joshk/Projects/MyEfficiencyTools/FullyCustom_xremap.yml --device /dev/input/event4
 # How to use:
 # Install xremap
+# If you installed using rustup & cargo, you need to add ~/.cargo/bin to ~/.profile or ~/.bashrc (or whatever shell in use)
 # Install crontab (whatever flavor, EOS seems to use cronie (yay cron, get cronie))
 # Activate crontab using systemctl
     # Just because it's installed doesn't mean it's active
@@ -8,3 +10,4 @@ bash xremap /home/joshk/Projects/MyEfficiencyTools/FullyCustom_xremap.yml --devi
     # sudo systemctl start cronie
 # use "sudo EDITOR=nano crontab -e" to make an entry for "@reboot /home/joshk/Projects/MyEfficiencyTools/BootCronScript.sh"
 # add write permissions to this file by sudo chmod +x BootCronScript.sh
+# Be sure the permissions include minimum of wrx,rx,rx for both yml and .sh
